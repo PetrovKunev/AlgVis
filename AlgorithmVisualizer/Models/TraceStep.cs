947f4c2491b29faf992777a7a2bc628a742163ae
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2025-07-14 13:42:51 +0300

    Remove AlgorithmVisualizer project and related files
    Deleted the entire AlgorithmVisualizer project, including all source code, components, services, configuration files, and static assets. Also updated .gitignore

--- AlgorithmVisualizer/Models/TraceStep.cs
+++
@@ -1,9 +0,0 @@
-namespace AlgorithmVisualizer.Models;
-
-public class TraceStep
-{
-    public int StepNumber { get; set; }
-    public string Action { get; set; } = string.Empty;
-    public object State { get; set; } = new();
-    public string Description { get; set; } = string.Empty;
-} 
--- AlgorithmVisualizer/Services/ITraceGenerator.cs
+++
@@ -1,9 +0,0 @@
-using AlgorithmVisualizer.Models;
-
-namespace AlgorithmVisualizer.Services;
-
-public interface ITraceGenerator
-{
-    string AlgorithmName { get; }
-    List<TraceStep> GenerateTrace(object input);
-} 
